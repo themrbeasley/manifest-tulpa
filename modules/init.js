132e0d23cb5f09261547981ee9437c4d8d570eb6
themrbeasley <118113229+themrbeasley@users.noreply.github.com>
2026-05-24 23:43:50 -0400

    feat: add Relentless watcher with reload-time restore

--- modules/init.js
+++ modules/init.js
@@ -1,13 +1,15 @@
 import { MODULE_ID } from "./constants.js";
 import { onPostUseActivity } from "./cast-flow.js";
 import { onCombatStart } from "./initiative.js";
+import { restoreRelentlessWatchers } from "./relentless-watcher.js";
 
 Hooks.once("init", () => {
   console.log(`${MODULE_ID} | init`);
 });
 
-Hooks.once("ready", () => {
+Hooks.once("ready", async () => {
   console.log(`${MODULE_ID} | ready`);
   Hooks.on("dnd5e.postUseActivity", onPostUseActivity);
   Hooks.on("combatStart", onCombatStart);
+  await restoreRelentlessWatchers();
 });
